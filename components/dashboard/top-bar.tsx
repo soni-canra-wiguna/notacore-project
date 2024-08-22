8dbb51c71764bac4df8683d19a815fdfe4ba9621
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-08-22 15:51:29 +0700

    feat: add border

--- components/dashboard/top-bar.tsx
+++ components/dashboard/top-bar.tsx
@@ -6,15 +6,33 @@ import { cn } from "@/lib/utils"
 import Account from "./account"
 import { SearchBar } from "./search"
 import { Bell } from "lucide-react"
+import { useEffect, useState } from "react"
 
 export const TopBar = ({ token }: { token: string }) => {
   const { visible } = useVisibleNavbar()
+  const [isBorderVisible, setIsBorderVisible] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 90) {
+        setIsBorderVisible(true)
+      } else {
+        setIsBorderVisible(false)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
   return (
     <nav
       className={cn(
         "fixed left-0 top-0 z-50 h-max w-full bg-background transition duration-300 ease-in-out",
         visible ? "translate-y-0 opacity-100" : "-translate-y-20 opacity-0",
+        isBorderVisible ? "border-b" : "border-none",
       )}
     >
       <Container className="flex items-center gap-6 py-3">
