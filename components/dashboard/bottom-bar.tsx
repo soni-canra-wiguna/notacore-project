331a4abc90344d659f6eeac60458b96561682ce7
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-15 16:47:28 +0700

    refactor: add path property to handle page active

--- components/dashboard/bottom-bar.tsx
+++ components/dashboard/bottom-bar.tsx
@@ -6,27 +6,33 @@ import { Container } from "../layout/container"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { cn } from "@/lib/utils"
-
-const navigationItems = [
-  {
-    label: "beranda",
-    icon: Home,
-    href: "/dashboard",
-  },
-  {
-    label: "tambah",
-    icon: PlusCircle,
-    href: "/dashboard/create-product",
-  },
-  {
-    label: "statistik",
-    icon: ChartArea,
-    href: "/dashboard/statistics",
-  },
-]
+import { dateTime } from "@/utils/date-time"
 
 export const BottomBar = () => {
   const pathname = usePathname()
+  const { formatToday } = dateTime()
+
+  const navigationItems = [
+    {
+      label: "beranda",
+      icon: Home,
+      path: "/dashboard",
+      href: "/dashboard",
+    },
+    {
+      label: "tambah",
+      icon: PlusCircle,
+      path: "/dashboard/create-product",
+      href: "/dashboard/create-product",
+    },
+    {
+      label: "statistik",
+      icon: ChartArea,
+      path: `/dashboard/statistics`,
+      href: `/dashboard/statistics?from=${formatToday.format1}&to=${formatToday.format1}`,
+    },
+  ]
+
   return (
     <header className="fixed bottom-0 left-0 z-50 w-full border-t bg-background">
       <Container className="flex h-full items-center justify-between px-16 py-2">
@@ -37,7 +43,7 @@ export const BottomBar = () => {
               href={item.href}
               className={cn(
                 "flex flex-col items-center gap-1.5",
-                pathname === item.href && "text-main hover:text-main",
+                pathname === item.path && "text-main hover:text-main",
               )}
             >
               <item.icon className="size-5 stroke-[1.5]" />
