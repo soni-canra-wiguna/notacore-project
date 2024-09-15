111446d37a3f4ae227d7f2f638a8e499b2e9a1a0
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-09-15 17:13:56 +0700

    feat(ui): add space

--- components/dashboard/statistic/amount.tsx
+++ components/dashboard/statistic/amount.tsx
@@ -32,10 +32,11 @@ const Amount = () => {
   if (!statistic || data?.data.length <= 0)
     return <ErrorAmount title="Belum ada yang terjual nih" />
 
-  if (isError) return <ErrorAmount title="Internet kamu lemot :/" />
+  if (isError)
+    return <ErrorAmount title="Internet kamu lemot, coba deh refresh lagi" />
 
   return (
-    <>
+    <div className="space-y-2">
       <div className="grid grid-cols-2 gap-2">
         <Card className="gradientCard flex flex-col gap-1 rounded-xl p-4">
           <span className="text-xs capitalize text-muted-foreground">
@@ -79,7 +80,7 @@ const Amount = () => {
         </Card>
       </div>
       <AmountByCategory categories={statistic?.salesAndRevenueByCategory} />
-    </>
+    </div>
   )
 }
 
@@ -178,54 +179,52 @@ export const AmountByCategory = ({
     }
   }
   return (
-    <div className="mt-2">
-      <Carousel
-        opts={{ align: "center", loop: true }}
-        setApi={setApi}
-        plugins={[
-          Autoplay({
-            delay: 4000,
-          }) as any,
-        ]}
-      >
-        <CarouselContent>
-          {categories?.map((category, i) => (
-            <CarouselItem key={i}>
-              <Card className="gradientCard grid h-[130px] w-full grid-cols-2 overflow-hidden rounded-xl selection:bg-transparent">
-                <div className="flex flex-col gap-1 p-4">
-                  <Badge variant="secondary" className="mb-3 w-max">
-                    {category.label}
-                  </Badge>
-                  <span className="mb-1 text-xs capitalize text-muted-foreground">
-                    total terjual
-                  </span>
-                  <span className="text-lg font-bold leading-none">
-                    {category.quantity}
-                  </span>
-                </div>
-                <div className="flex items-center justify-center">
-                  <span className="text-lg font-bold leading-none">
-                    {formatToIDR(category.totalPrice)}
-                  </span>
-                </div>
-              </Card>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <div className="absolute bottom-3 right-3 z-30 flex items-center gap-1.5">
-          {Array.from({ length: count }, (_, i) => {
-            return (
-              <span
-                key={i}
-                onClick={() => handleDotClick(i)}
-                className={`h-[4px] w-6 cursor-pointer rounded-full ${
-                  current === i + 1 ? "bg-primary" : "bg-muted-foreground/50"
-                }`}
-              />
-            )
-          })}
-        </div>
-      </Carousel>
-    </div>
+    <Carousel
+      opts={{ align: "center", loop: true }}
+      setApi={setApi}
+      plugins={[
+        Autoplay({
+          delay: 4000,
+        }) as any,
+      ]}
+    >
+      <CarouselContent>
+        {categories?.map((category, i) => (
+          <CarouselItem key={i}>
+            <Card className="gradientCard grid h-[130px] w-full grid-cols-2 overflow-hidden rounded-xl selection:bg-transparent">
+              <div className="flex flex-col gap-1 p-4">
+                <Badge variant="secondary" className="mb-3 w-max">
+                  {category.label}
+                </Badge>
+                <span className="mb-1 text-xs capitalize text-muted-foreground">
+                  total terjual
+                </span>
+                <span className="text-lg font-bold leading-none">
+                  {category.quantity}
+                </span>
+              </div>
+              <div className="flex items-center justify-center">
+                <span className="text-lg font-bold leading-none">
+                  {formatToIDR(category.totalPrice)}
+                </span>
+              </div>
+            </Card>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <div className="absolute bottom-3 right-3 z-30 flex items-center gap-1.5">
+        {Array.from({ length: count }, (_, i) => {
+          return (
+            <span
+              key={i}
+              onClick={() => handleDotClick(i)}
+              className={`h-[4px] w-6 cursor-pointer rounded-full ${
+                current === i + 1 ? "bg-primary" : "bg-muted-foreground/50"
+              }`}
+            />
+          )
+        })}
+      </div>
+    </Carousel>
   )
 }
