06d3ba5a6da1f41b10f0ef2fca891c702aa1549a
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-10-03 12:58:21 +0700

    feat(api): add API to delete user from clerk and database

--- app/api/users/[userId]/route.ts
+++ app/api/users/[userId]/route.ts
@@ -1,5 +1,6 @@
 import prisma from "@/lib/prisma"
 import { NextRequest, NextResponse } from "next/server"
+import { clerkClient } from "@clerk/nextjs/server"
 
 export const GET = async (
   req: NextRequest,
@@ -28,6 +29,56 @@ export const GET = async (
 
     return NextResponse.json(
       { message: "user was retrieved", data: user },
+      { status: 200 },
+    )
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json(
+      {
+        message: "Internal server error",
+      },
+      {
+        status: 500,
+      },
+    )
+  }
+}
+
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { userId: string } },
+) => {
+  try {
+    const { userId } = params
+    if (!userId) {
+      return NextResponse.json(
+        { message: "Unauthorized. User not Found." },
+        { status: 404 },
+      )
+    }
+
+    const token = req.headers.get("authorization")
+    if (!token) {
+      return NextResponse.json(
+        { message: "Unauthorized. No token provided." },
+        { status: 401 },
+      )
+    }
+
+    // delete user from clerk
+    await clerkClient().users.deleteUser(userId)
+
+    // delete user from database
+    await prisma.user.delete({
+      where: {
+        userId,
+      },
+    })
+
+    return NextResponse.json(
+      {
+        message: "user deleted",
+      },
       { status: 200 },
     )
   } catch (error) {
