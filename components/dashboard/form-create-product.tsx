172dcda49d94328c8f72dee409e9810d38889b28
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-10-04 07:50:05 +0700

    refactor: reusable form

--- components/dashboard/form-create-product.tsx
+++ components/dashboard/form-create-product.tsx
@@ -1,14 +1,6 @@
 "use client"
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { ProductValidation } from "@/schema/product.schema"
+import { InferProductType, ProductValidation } from "@/schema/product.schema"
 import React from "react"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -17,27 +9,8 @@ import axios from "axios"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { toast } from "@/components/ui/use-toast"
-import { Input } from "@/components/ui/input"
-import { FileUpload } from "@/components/file-upload"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { UNIT_PRODUCTS } from "@/constants/units"
-import { Label } from "@/components/ui/label"
-import { formatToIDR } from "@/utils/format-to-idr"
-import LoadingButton from "@/components/loading-button"
-import { Button } from "../ui/button"
-import { FileSearch } from "lucide-react"
-import { PreviewDetailProduct } from "./preview-product"
 import { v4 as uuidv4 } from "uuid"
-import { TextEditor } from "../text-editor"
-
-type InferCreateProduct = z.infer<typeof ProductValidation.CREATE>
+import { ProductForm } from "./product-form"
 
 export const FormCreateProduct = ({
   userId,
@@ -49,18 +22,20 @@ export const FormCreateProduct = ({
   const router = useRouter()
   const queryClient = useQueryClient()
 
-  const form = useForm<InferCreateProduct>({
+  const defaultValues = {
+    userId: userId!,
+    title: "",
+    description: "",
+    image: "",
+    price: 0,
+    category: "",
+    stock: 0,
+    unit: "PCS",
+  }
+
+  const form = useForm<InferProductType>({
     resolver: zodResolver(ProductValidation.CREATE),
-    defaultValues: {
-      userId: userId!,
-      title: "",
-      description: "",
-      image: "",
-      price: 0,
-      category: "",
-      stock: 0,
-      unit: "PCS",
-    },
+    defaultValues,
   })
 
   const {
@@ -68,7 +43,7 @@ export const FormCreateProduct = ({
     mutate: createProduct,
     isError,
   } = useMutation({
-    mutationFn: async (data: InferCreateProduct) => {
+    mutationFn: async (data: InferProductType) => {
       await axios.post("/api/products", data, {
         headers: {
           "Content-Type": "application/json",
@@ -103,12 +78,8 @@ export const FormCreateProduct = ({
     },
   })
 
-  const onSubmit = (data: InferCreateProduct) => {
-    try {
-      createProduct(data)
-    } catch (error) {
-      console.log("[FAILED TO SUBMIT PRODUCT]", error)
-    }
+  const onSubmit = (data: InferProductType) => {
+    createProduct(data)
   }
 
   const previewProduct = {
@@ -126,183 +97,12 @@ export const FormCreateProduct = ({
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="mx-auto w-full space-y-6"
-      >
-        <FormField
-          control={form.control}
-          name="image"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Image</FormLabel>
-                <FormControl>
-                  <FileUpload
-                    endpoint="product"
-                    value={field.value}
-                    onChange={field.onChange}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Title produk</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="masukkan nama produk"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Deskripsi produk</FormLabel>
-                <FormControl>
-                  <TextEditor value={field.value!} onChange={field.onChange} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <div className="flex w-full items-start gap-4">
-          <FormField
-            control={form.control}
-            name="price"
-            render={({ field }) => {
-              return (
-                <FormItem className="flex-1">
-                  <FormLabel>Harga produk</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="masukkan nominal harga"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-          <div className="space-y-2">
-            <Label>Preview harga</Label>
-            <div className="flex h-10 w-full items-center rounded-xl bg-secondary px-4">
-              {formatToIDR(form.watch("price"))}
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full items-start gap-4">
-          <FormField
-            control={form.control}
-            name="stock"
-            render={({ field }) => {
-              return (
-                <FormItem className="flex-1">
-                  <FormLabel>Jumlah produk</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="jumlah produk tersedia"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-          {/* unit / satuan */}
-          <FormField
-            control={form.control}
-            name="unit"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Satuan</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="satuan produk" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent align="end">
-                      <SelectGroup>
-                        {UNIT_PRODUCTS.map((unit) => (
-                          <SelectItem
-                            className="capitalize"
-                            key={unit.value}
-                            value={unit.value}
-                          >
-                            {unit.label}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="category"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>kategori produk</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="masukkan kategori"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <div className="flex flex-col gap-2.5 pt-3">
-          <LoadingButton
-            type="submit"
-            loading={isPending}
-            disabled={isPending}
-            className="w-full capitalize"
-          >
-            tambah produk
-          </LoadingButton>
-          <PreviewDetailProduct product={previewProduct}>
-            <Button className="w-full" variant="outline">
-              <FileSearch className="mr-2 size-4" /> preview
-            </Button>
-          </PreviewDetailProduct>
-        </div>
-      </form>
-    </Form>
+    <ProductForm
+      form={form}
+      previewProduct={previewProduct}
+      isPending={isPending}
+      onSubmit={onSubmit}
+      label="update"
+    />
   )
 }
--- components/dashboard/form-edit-product.tsx
+++ components/dashboard/form-edit-product.tsx
@@ -1,14 +1,6 @@
 "use client"
 
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { ProductValidation } from "@/schema/product.schema"
+import { InferProductType, ProductValidation } from "@/schema/product.schema"
 import React from "react"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -17,28 +9,9 @@ import axios from "axios"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { toast } from "@/components/ui/use-toast"
-import { Input } from "@/components/ui/input"
-import { FileUpload } from "@/components/file-upload"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { UNIT_PRODUCTS } from "@/constants/units"
-import { Label } from "@/components/ui/label"
-import { formatToIDR } from "@/utils/format-to-idr"
-import LoadingButton from "@/components/loading-button"
-import { Button } from "../ui/button"
-import { FileSearch } from "lucide-react"
-import { PreviewDetailProduct } from "./preview-product"
 import { v4 as uuidv4 } from "uuid"
 import { Product } from "@prisma/client"
-import { TextEditor } from "../text-editor"
-
-type InferUpdateProduct = z.infer<typeof ProductValidation.UPDATE>
+import { ProductForm } from "./product-form"
 
 export const FormEditProduct = ({
   token,
@@ -61,8 +34,8 @@ export const FormEditProduct = ({
     unit: product.unit!,
   }
 
-  const form = useForm<InferUpdateProduct>({
-    resolver: zodResolver(ProductValidation.UPDATE),
+  const form = useForm<InferProductType>({
+    resolver: zodResolver(ProductValidation.CREATE),
     defaultValues,
   })
 
@@ -71,7 +44,7 @@ export const FormEditProduct = ({
     mutate: createProduct,
     isError,
   } = useMutation({
-    mutationFn: async (data: InferUpdateProduct) => {
+    mutationFn: async (data: InferProductType) => {
       await axios.patch(`/api/products/${product.id}`, data, {
         headers: {
           "Content-Type": "application/json",
@@ -98,12 +71,8 @@ export const FormEditProduct = ({
     },
   })
 
-  const onSubmit = (data: InferUpdateProduct) => {
-    try {
-      createProduct(data)
-    } catch (error) {
-      console.log("[FAILED TO SUBMIT PRODUCT]", error)
-    }
+  const onSubmit = (data: InferProductType) => {
+    createProduct(data)
   }
 
   const previewProduct = {
@@ -121,183 +90,12 @@ export const FormEditProduct = ({
   }
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="mx-auto w-full space-y-6"
-      >
-        <FormField
-          control={form.control}
-          name="image"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Image</FormLabel>
-                <FormControl>
-                  <FileUpload
-                    endpoint="product"
-                    value={field.value!}
-                    onChange={field.onChange}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Title produk</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="masukkan nama produk"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>Deskripsi produk</FormLabel>
-                <FormControl>
-                  <TextEditor value={field.value!} onChange={field.onChange} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <div className="flex w-full items-start gap-4">
-          <FormField
-            control={form.control}
-            name="price"
-            render={({ field }) => {
-              return (
-                <FormItem className="flex-1">
-                  <FormLabel>Harga produk</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="masukkan nominal harga"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-          <div className="space-y-2">
-            <Label>Preview harga</Label>
-            <div className="flex h-10 w-full items-center rounded-xl bg-secondary px-4">
-              {formatToIDR(form.watch("price")!)}
-            </div>
-          </div>
-        </div>
-        <div className="flex w-full items-start gap-4">
-          <FormField
-            control={form.control}
-            name="stock"
-            render={({ field }) => {
-              return (
-                <FormItem className="flex-1">
-                  <FormLabel>Jumlah produk</FormLabel>
-                  <FormControl>
-                    <Input
-                      type="number"
-                      placeholder="jumlah produk tersedia"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-          {/* unit / satuan */}
-          <FormField
-            control={form.control}
-            name="unit"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Satuan</FormLabel>
-                  <Select
-                    onValueChange={field.onChange}
-                    defaultValue={field.value}
-                  >
-                    <FormControl>
-                      <SelectTrigger>
-                        <SelectValue placeholder="satuan produk" />
-                      </SelectTrigger>
-                    </FormControl>
-                    <SelectContent align="end">
-                      <SelectGroup>
-                        {UNIT_PRODUCTS.map((unit) => (
-                          <SelectItem
-                            className="capitalize"
-                            key={unit.value}
-                            value={unit.value}
-                          >
-                            {unit.label}
-                          </SelectItem>
-                        ))}
-                      </SelectGroup>
-                    </SelectContent>
-                  </Select>
-                  <FormMessage />
-                </FormItem>
-              )
-            }}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="category"
-          render={({ field }) => {
-            return (
-              <FormItem>
-                <FormLabel>kategori produk</FormLabel>
-                <FormControl>
-                  <Input
-                    type="text"
-                    placeholder="masukkan kategori"
-                    {...field}
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )
-          }}
-        />
-        <div className="flex flex-col gap-2.5 pt-3">
-          <LoadingButton
-            type="submit"
-            loading={isPending}
-            disabled={isPending}
-            className="w-full capitalize"
-          >
-            update produk
-          </LoadingButton>
-          <PreviewDetailProduct product={previewProduct}>
-            <Button className="w-full" variant="outline">
-              <FileSearch className="mr-2 size-4" /> preview
-            </Button>
-          </PreviewDetailProduct>
-        </div>
-      </form>
-    </Form>
+    <ProductForm
+      form={form}
+      previewProduct={previewProduct}
+      isPending={isPending}
+      onSubmit={onSubmit}
+      label="update"
+    />
   )
 }
