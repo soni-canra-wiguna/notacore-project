8d6d18c42554acbc10af16c457ca88428db414fe
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-10-05 21:59:20 +0700

    feat: add download option, cancel using useQuery

--- components/dashboard/statistic/download-transaction-history.tsx
+++ components/dashboard/statistic/download-transaction-history.tsx
@@ -1,44 +1,40 @@
 "use client"
 
+import { FileType } from "@/app/api/sale-records/download/[userId]/route"
 import { Button } from "@/components/ui/button"
+import { Card } from "@/components/ui/card"
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
 import { useAuth } from "@clerk/nextjs"
-import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
-import { Download, Loader2 } from "lucide-react"
+import { Download, FileDown, X } from "lucide-react"
 
 export const DownloadTransactionHistory = () => {
   const { userId } = useAuth()
 
-  // fetch data
-  const fetchCSVData = async () => {
-    const { data } = await axios.get(`/api/sale-records/download/${userId}`, {
-      responseType: "text",
-    })
-    return data
-  }
-
-  const {
-    data: csv,
-    isPending,
-    isError,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["downloadProducts"],
-    queryFn: fetchCSVData,
-  })
-
-  async function handleDownload() {
+  async function handleDownload(type: FileType) {
     try {
-      await refetch() // Manually trigger fetching when button is clicked
-      if (!csv) return
+      const { data } = await axios.get(`/api/sale-records/download/${userId}?fileType=${type}`, {
+        responseType: "blob",
+      })
 
-      // Trigger download after data is fetched
-      const blob = new Blob([csv], { type: "text/csv;charset=utf-8;" })
+      const blob = new Blob([data], {
+        type:
+          type === "csv"
+            ? "text/csv"
+            : "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      })
       const url = window.URL.createObjectURL(blob)
       const a = document.createElement("a")
       a.href = url
-      a.download = "sales-record.csv"
+      a.download = `sales-record.${type}`
       document.body.appendChild(a)
       a.click()
       document.body.removeChild(a)
@@ -48,19 +44,51 @@ export const DownloadTransactionHistory = () => {
   }
 
   return (
-    <Button
-      onClick={() => handleDownload()}
-      disabled={isPending}
-      variant="outline"
-      size="icon"
-      className="h-9 w-fit gap-2 p-2"
+    <Drawer open>
+      <DrawerTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Download className="size-4 stroke-[1.5]" />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="flex items-center justify-between border-b">
+          <DrawerTitle className="sr-only">Download</DrawerTitle>
+          <DrawerDescription className="sr-only">Download Transaksi Penjualan</DrawerDescription>
+          <div className="flex items-center gap-3">
+            <h3 className="font-semibold capitalize">Download</h3>
+          </div>
+          <DrawerClose asChild>
+            <Button variant="ghost" size="icon">
+              <X className="size-6 stroke-[1.5]" />
+              <p className="sr-only">close</p>
+            </Button>
+          </DrawerClose>
+        </DrawerHeader>
+        <div className="grid w-full grid-cols-2 gap-4 p-4">
+          <DownloadButton fileType="csv" onClick={() => handleDownload("csv")} />
+          <DownloadButton fileType="xlsx" onClick={() => handleDownload("xlsx")} />
+        </div>
+      </DrawerContent>
+    </Drawer>
+  )
+}
+
+interface DownloadButtonProps {
+  fileType: FileType
+  onClick: React.MouseEventHandler<HTMLDivElement> | undefined
+}
+
+const DownloadButton: React.FC<DownloadButtonProps> = ({ fileType, onClick }) => {
+  return (
+    <Card
+      onClick={onClick}
+      className="gradientCard flex w-full cursor-pointer items-center gap-2 rounded-xl p-3"
     >
-      {isPending ? (
-        <Loader2 className="size-4 animate-spin stroke-[1.5]" />
-      ) : (
-        <Download className="size-4 stroke-[1.5]" />
-      )}
-      <span className="capitalize">download</span>
-    </Button>
+      <FileDown className="size-10 stroke-[1.5] text-main" />
+      <div className="">
+        <h1 className="text-base font-semibold uppercase leading-none">{fileType}</h1>
+        <span className="text-sm text-muted-foreground">sales-record.{fileType}</span>
+      </div>
+    </Card>
   )
 }
