284a92bf18d5f287419fd2d06d2f446a2e7a2bc7
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-10-06 14:40:59 +0700

    perf: separate into new components

--- components/dashboard/filter-products.tsx
+++ components/dashboard/filter-products.tsx
@@ -12,7 +12,7 @@ import {
 } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
-import { useQueryState } from "nuqs"
+import { Options, useQueryState } from "nuqs"
 import {
   Drawer,
   DrawerContent,
@@ -22,55 +22,67 @@ import {
   DrawerTrigger,
 } from "@/components/ui/drawer"
 
+const filterItems = [
+  {
+    label: "terbaru",
+    value: "new",
+    icon: ArrowUpDown,
+  },
+  {
+    label: "terlama",
+    value: "old",
+    icon: ArrowDownUp,
+  },
+  {
+    label: "a - z",
+    value: "a-z",
+    icon: ArrowDownAZ,
+  },
+  {
+    label: "z - a",
+    value: "z-a",
+    icon: ArrowDownZA,
+  },
+  {
+    label: "harga : termurah",
+    value: "price-low",
+    icon: ArrowDownUp,
+  },
+  {
+    label: "harga : termahal",
+    value: "price-high",
+    icon: ArrowUpDown,
+  },
+  {
+    label: "Stock : terbanyak",
+    value: "stock-high",
+    icon: ArrowUp10,
+  },
+  {
+    label: "stock : tersedikit",
+    value: "stock-low",
+    icon: ArrowDown01,
+  },
+]
+
+type FilterItem = (typeof filterItems)[0]
+type FilterItemProps = {
+  item: FilterItem
+  sortBy: string
+  isOpen: boolean
+  setIsOpen: (isOpen: boolean) => void
+  setSortBy: <Shallow>(
+    value: string | ((old: string) => string | null) | null,
+    options?: Options<Shallow> | undefined,
+  ) => Promise<URLSearchParams>
+}
+
 export const FilterProducts = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [sortBy, setSortBy] = useQueryState("sortBy", {
     history: "push",
     defaultValue: "new",
   })
-
-  const filterItems = [
-    {
-      label: "terbaru",
-      value: "new",
-      icon: ArrowUpDown,
-    },
-    {
-      label: "terlama",
-      value: "old",
-      icon: ArrowDownUp,
-    },
-    {
-      label: "a - z",
-      value: "a-z",
-      icon: ArrowDownAZ,
-    },
-    {
-      label: "z - a",
-      value: "z-a",
-      icon: ArrowDownZA,
-    },
-    {
-      label: "harga : termurah",
-      value: "price-low",
-      icon: ArrowDownUp,
-    },
-    {
-      label: "harga : termahal",
-      value: "price-high",
-      icon: ArrowUpDown,
-    },
-    {
-      label: "Stock : terbanyak",
-      value: "stock-high",
-      icon: ArrowUp10,
-    },
-    {
-      label: "stock : tersedikit",
-      value: "stock-low",
-      icon: ArrowDown01,
-    },
-  ]
 
   return (
     <Drawer open={isOpen} onOpenChange={setIsOpen}>
@@ -82,38 +94,49 @@ export const FilterProducts = () => {
       <DrawerContent className="">
         <DrawerHeader className="sr-only">
           <DrawerTitle>filter produk</DrawerTitle>
-          <DrawerDescription>
-            filter produk dengan berbagai pilihan
-          </DrawerDescription>
+          <DrawerDescription>filter produk dengan berbagai pilihan</DrawerDescription>
         </DrawerHeader>
         <div className="scrollbar-hide h-full max-h-[450px] w-full overflow-y-auto p-4">
           <div className="flex flex-col gap-2">
             {filterItems.map((item) => (
-              <button
-                onClick={() => {
-                  setSortBy(item.value)
-                  setIsOpen(!isOpen)
-                }}
+              <FilterItem
                 key={item.label}
-                className={cn(
-                  "capitaize group relative flex w-full items-center gap-3 rounded-md px-4 py-2 text-left text-sm capitalize hover:bg-main/10",
-                  sortBy === item.value && "bg-main/10",
-                )}
-              >
-                <span
-                  className={cn(
-                    "absolute left-0 top-1/2 hidden h-6 w-1 -translate-y-1/2 rounded-lg bg-main group-hover:inline",
-                    sortBy === item.value && "inline",
-                  )}
-                />
-                <item.icon className="size-4 stroke-[1.5]" />
-                {item.label}
-              </button>
+                item={item}
+                sortBy={sortBy}
+                setSortBy={setSortBy}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+              />
             ))}
           </div>
         </div>
       </DrawerContent>
     </Drawer>
+  )
+}
+
+const FilterItem = ({ item, sortBy, setSortBy, isOpen, setIsOpen }: FilterItemProps) => {
+  return (
+    <button
+      onClick={() => {
+        setSortBy(item.value)
+        setIsOpen(!isOpen)
+      }}
+      key={item.label}
+      className={cn(
+        "capitaize group relative flex w-full items-center gap-3 rounded-md px-4 py-2 text-left text-sm capitalize hover:bg-main/10",
+        sortBy === item.value && "bg-main/10",
+      )}
+    >
+      <span
+        className={cn(
+          "absolute left-0 top-1/2 hidden h-6 w-1 -translate-y-1/2 rounded-lg bg-main group-hover:inline",
+          sortBy === item.value && "inline",
+        )}
+      />
+      <item.icon className="size-4 stroke-[1.5]" />
+      {item.label}
+    </button>
   )
 }
 
