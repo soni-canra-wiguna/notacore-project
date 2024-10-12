b01d958fe49d4a1f9bfe96f380716906800f361b
soni-canra-wiguna <sonicandra0511@gmail.com>
2024-10-12 20:43:57 +0700

    fix: change protected route

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
-const isProtectedRoute = createRouteMatcher(["/nothing(.*)"])
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"])
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) {
